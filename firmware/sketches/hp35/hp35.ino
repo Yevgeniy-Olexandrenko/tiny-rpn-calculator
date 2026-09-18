b537e641a3da2287b7c0975893a7f065e425806c
Yevgeniy <yevgeniy.olexandrenko@gmail.com>
2026-09-19 02:36:12 +0300

    Refactoring and cleanup

--- firmware/sketches/hp35/hp35.ino
+++ firmware/sketches/hp35/hp35.ino
@@ -2,8 +2,7 @@
 // Emulator for HP-35 Calculator 
 // -----------------------------------------------------------------------------
 
-#define  PCB_REV 13
-#define  ENABLE_OPT_DATAMEM  0
+#define  ENABLE_OPT_DATAMEM  1
 #define  ENABLE_OPT_NOINLINE 1
 
 #include <TinyRPNCalculator.h>
@@ -14,9 +13,5 @@
 #include "code.h"
 
 // Reference compilation output:
-// Sketch uses 7296 bytes (89%) of program storage space. Maximum is 8192 bytes.
-// Global variables use 204 bytes (39%) of dynamic memory, leaving 308 bytes for local variables. Maximum is 512 bytes.
-
-// New reference compilation output:
-// Sketch uses 6854 bytes (83%) of program storage space. Maximum is 8192 bytes.
+// Sketch uses 6662 bytes (81%) of program storage space. Maximum is 8192 bytes.
 // Global variables use 187 bytes (36%) of dynamic memory, leaving 325 bytes for local variables. Maximum is 512 bytes.
